2bcb02433a2ba001c6a8236b97b80c8944a2c83a
Alexander Berezovsky <a.b@usa.com>
2013-11-23 16:20:59 -0800

    fix coveralls loading

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,17 @@
+require 'coveralls'
+Coveralls.wear!
+
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
 require 'hall'
 require 'rspec'
 require 'rspec/autorun'
 require 'vcr'
 require 'webmock'
-require 'coveralls'
 
 VCR.configure do |c|
   c.cassette_library_dir = 'fixtures/vcr_cassettes'
   c.hook_into :webmock
 end
 
-Coveralls.wear!
